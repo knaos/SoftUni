cb2c1fb90fee5b4ca40be70ee6e2c386ffcfcb88
knaos <akaynarov@gmail.com>
2015-04-16 13:15:44 +0300

    Added js apps

--- JsApps/Homework Web Storages/POLL/scripts.js
+++ JsApps/Homework Web Storages/POLL/scripts.js
@@ -1,7 +1,72 @@
-var poll = (function  () {
+if (!localStorage.submited) {
+	localStorage.setItem('submited', false);
+}
+
+if (!localStorage.checkedItems) {
+	localStorage.setItem('checkedItems', []);
+}
+
+	var question = new Question('who is the president', 'me','you', 'nakov', 'mama');
+	var question2 = new Question('what are you', 'alien','dog', 'plane', 'programer');
+
+	question.addQuestion();
+	question2.addQuestion();
+
+
+fillRadio();
+
+
+
+
+
+$('#poll').append('<button>Submit</button>');
+
+$('button').on('click', function  (ev) {
+	localStorage.setItem('submited', true);
+});
+
+$('input').on('click', saveItems);
+
+function saveItems (ev) {
+	var arr = [];
+	$('input:checked').each(function  () {
+		arr.push(this.value);
+	});
+	localStorage.setItem('checkedItems', JSON.stringify(arr));
+}
+
+function fillRadio () {
+	var inputs = $('input');
+	inputs.each(function  () {
+		var that = this;
+		var value = this.value;
+		JSON.parse(localStorage.checkedItems).forEach(function  (element) {
+			if(value === element){
+				that.checked = true;
+			}
+		});
+	});
 	
-	
-	function setTimer () {
-		
-	}
-})();
+}
+
+var maxtime = 5 * 60;
+countdown();
+var interval = setInterval(countdown, 1000);
+function countdown (){
+	var minutes,
+		seconds;
+	maxtime--;
+
+	minutes = Math.floor(maxtime / 60);
+	seconds = Math.floor(maxtime % 60);
+
+	if (seconds < 10) {
+		seconds = '0' + seconds;
+	};
+	if (minutes < 10) {
+		minutes = '0' + minutes;
+	};
+
+	$('#timer').text(minutes + ':' + seconds);
+
+}
